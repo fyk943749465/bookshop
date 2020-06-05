05484d02552aff81720f34c03060a0345e467489
codeBirde <fyk943749465@gmail.com>
2020-06-05 17:13:57 +0800

    jpa dynamic query

--- src/main/java/com/store/repository/BookRepository.java
+++ src/main/java/com/store/repository/BookRepository.java
@@ -2,16 +2,38 @@ package com.store.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.store.domain.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book>{
 
 	// CrudRepository 增删改查的一些方法
 	// PagingAndSortingRepository 增加了带分页的查询
 	// JpaRepository 最长继承的一个接口。 getOne 方法
+	// JpaSpecificationExecutor 该接口是生成动态查询的接口，上面的都是静态查询接口，所以，一般一个Repository要继承静态查询和动态查询接口
+	// 静态查询
 	List<Book> findByName(String name);
-	
+	// 静态查询
 	List<Book> findByNameAndCategoryName(String bookName, String categoryName);
+	// 静态查询，根据属性来查询
+    Page<Book> findByNameLikeAndCategoryNameOrderByNameDesc(String bookname, String category, Pageable pageable);
+    
+    //JPAQL 静态查询，用JPAQL来查询
+    @Query("from Book b where b.name like ?1 and b.category.name = ?2 order by b.name desc")
+    Page<Book> findBooks(String bookname, String categoryName, Pageable sort);
+    
+    @Query("update Book b set b.name = ?1 where b.id = ?2")
+    @Modifying  //该注解表明需要动库
+    int updateBooks(String bookname, Long bookId);
+    
+    // 原生sql
+    @Query(value = "update book b set b.name = ?1 where b.id = ?2", nativeQuery = true)
+    @Modifying
+    int updateBooks2(String bookname, Long bookId);
 }
--- src/test/java/com/store/repository/RepositoryTest.java
+++ src/test/java/com/store/repository/RepositoryTest.java
@@ -1,5 +1,11 @@
 package com.store.repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
@@ -10,6 +16,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
 
 import com.store.BaseTest;
 import com.store.domain.Book;
@@ -56,5 +64,25 @@ public class RepositoryTest extends BaseTest {
 //		Example<Book> example = Example.of(book, matcher);
 //		Page<Book> books = bookRepository.findAll(example, pageable);
 		
+//		Pageable pageable = PageRequest.of(0, 10, Sort.by(Direction.DESC, "name"));
+//		bookRepository.findByNameLikeAndCategoryNameOrderByNameDesc("战争与和平", "世界名著", pageable);
+		
+//		bookRepository.updateBooks("战争与和平", 1L);
+		
+		Specification<Book> specification = new Specification<Book>() {
+			@Override
+			public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+				// 1. 组合模式，自身即使容器又是实体
+				// 2. 可以根据前台传过来的参赛是否为空，来决定是否生成查询条件，达到动态查询的目的
+				Predicate p1 = criteriaBuilder.equal(root.get("name"), "战争与和平");
+				Predicate p2 = criteriaBuilder.equal(root.get("category").get("name"), "世界名著");
+				Predicate p3 = criteriaBuilder.and(p1, p2);
+				// 指定连接方式 ，如果不指定，jpa会自己生成
+				root.fetch("category", JoinType.LEFT);
+				return p3;
+			}
+		};
+		
+		bookRepository.findOne(specification);
 	}
 }
