a6484683f00b174f7bf51c856e2336dbff06b9e0
codeBirde <fyk943749465@gmail.com>
2020-06-05 16:38:39 +0800

    find by name

--- src/main/java/com/store/repository/BookRepository.java
+++ src/main/java/com/store/repository/BookRepository.java
@@ -2,11 +2,16 @@ package com.store.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.domain.Book;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
+	// CrudRepository 增删改查的一些方法
+	// PagingAndSortingRepository 增加了带分页的查询
+	// JpaRepository 最长继承的一个接口。 getOne 方法
 	List<Book> findByName(String name);
+	
+	List<Book> findByNameAndCategoryName(String bookName, String categoryName);
 }
--- src/test/java/com/store/repository/RepositoryTest.java
+++ src/test/java/com/store/repository/RepositoryTest.java
@@ -2,6 +2,14 @@ package com.store.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Sort.Direction;
 
 import com.store.BaseTest;
 import com.store.domain.Book;
@@ -16,8 +24,37 @@ public class RepositoryTest extends BaseTest {
 //		System.out.println(bookRepository.getClass().getName());
 //		bookRepository.findByName("战争与和平");
 		
-		Book book = new Book();
-		book.setName("name1");
-		bookRepository.save(book);
+//		bookRepository.findByNameAndCategoryName("战争与和平", "世界名著");
+		
+//		Book book = new Book();
+//		book.setName("name1");
+//		bookRepository.save(book);
+		
+//		List<Long> ids = new ArrayList<>();
+//		ids.add(1L);
+//		ids.add(2L);
+//		ids.add(3L);
+//		Iterable<Book> books = bookRepository.findAllById(ids);
+		
+//		System.out.println(bookRepository.count());
+
+//		Iterable<Book> findAll = bookRepository.findAll(Sort.by(Direction.DESC, "name", "id"));
+		
+//		Iterable<Book> findAll = bookRepository.findAll(Sort.by(Order.asc("name"), Order.desc("id")));
+		
+//		Pageable pageable = PageRequest.of(0, 10, Sort.by(Direction.DESC, "name"));
+//		Page<Book> books = bookRepository.findAll(pageable);
+		
+		
+//		Pageable pageable = PageRequest.of(0, 10, Sort.by(Direction.DESC, "name"));
+//		Book book = new Book();
+//		book.setName("战争");
+//		
+//		ExampleMatcher matcher = ExampleMatcher.matching()
+//				.withStringMatcher(StringMatcher.CONTAINING);
+//	
+//		Example<Book> example = Example.of(book, matcher);
+//		Page<Book> books = bookRepository.findAll(example, pageable);
+		
 	}
 }
